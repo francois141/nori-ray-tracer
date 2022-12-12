e0b299db71a5d324713059729972dd84cfcc2a65
francois141 <frankost.costa@gmail.com>
2022-12-12 17:14:35 +0100

    cleaning denoiser

--- denoiser/denoiser.py
+++ denoiser/denoiser.py
@@ -1,32 +1,35 @@
 import os
 os.environ["OPENCV_IO_ENABLE_OPENEXR"]="1"
-
 import cv2
 import math
 import numpy as np
 from scipy import signal
 import argparse
 
-PATH_TO_IMAGE:str = "image.exr"
-PATH_TO_IMAGE_VAR:str = "image_var.png"
+# Parse input path
+parser = argparse.ArgumentParser()
+parser.add_argument('--img_path', type=str, required=True, help='Image that should be denoised. Can be with .exr')
+parser.add_argument('--var_path', type=str, required=True, help='Image with pixel variance estimates. It must be png or jpg.')
+args = parser.parse_args()
 
-img = cv2.imread(PATH_TO_IMAGE, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
+# Read base image
+img = cv2.imread(args.img_path, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-img_variance = cv2.imread(PATH_TO_IMAGE_VAR)
+# Read pixel variance estimates
+img_variance = cv2.imread(args.var_path)
 img_variance = cv2.cvtColor(img_variance, cv2.COLOR_BGR2GRAY)
 
-x = img.shape[0]
-y = img.shape[1]
-
+# Base parameters for the algorithm
 epsilon = 1e-6
-k = 0.01
+k = 0.02
 
-r = 5
+r = 3
 flt = 0
 wgtsum = 0
-f = 5
+f = 10
 
+# Implementation of d2
 def d2(ngb,img,img_variance,k):
     v1 = img_variance + np.minimum(img_variance,ngb_variance)
     v2 = img_variance + ngb_variance
@@ -34,34 +37,48 @@ def d2(ngb,img,img_variance,k):
     sum /= epsilon + k*k*v2
     return sum
 
-def boxFilter(size):
-    size = 2*size+1
-    return np.ones(shape=(size,size)) / pow(size,2)
-
-flt = np.zeros(shape=img.shape)
-
+# Implementation of the shift function
 def shift(data,dx,dy):
     data = np.roll(data,dx,0)
     data = np.roll(data,dy,1)
     return data
 
-for dx in range(-r,r+1):
-    for dy in range(-r,r+1):
-        ngb = shift(img,dx,dy)
-        ngb_variance = shift(img_variance,dx,dy)
+# Creation of a simple box filter
+def boxFilter(size):
+    size = 2*size+1
+    return np.ones(shape=(size,size)) / pow(size,2)
 
-        d2pixel = d2(ngb,img,ngb_variance,k)
-        d2patch = signal.convolve2d(d2pixel,boxFilter(f-1),mode='same')
-        wgt = np.exp(-np.maximum(0,d2patch))
-        wgt = signal.convolve2d(wgt,boxFilter(f-1),mode='same')
-        flt += np.expand_dims(wgt,axis=2) * ngb
-        wgtsum += wgt
 
-flt /= np.expand_dims(wgtsum,axis=2)
+if __name__ == "__main__":
 
-cv2.imshow("denoised_image",flt)
-cv2.imshow("base_image",img)
-cv2.imshow("base_image_variance",img_variance)
+    # Creation of the base image
+    outputImage = np.zeros(shape=img.shape)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # Run the fast algorithm given in the slides
+    for dx in range(-r,r+1):
+        for dy in range(-r,r+1):
+
+            print("{} : {}".format(dx,dy))
+
+            ngb = shift(img,dx,dy)
+            ngb_variance = shift(img_variance,dx,dy)
+
+            d2pixel = d2(ngb,img,ngb_variance,k)
+            d2patch = signal.convolve2d(d2pixel,boxFilter(f-1),mode='same')
+            wgt = np.exp(-np.maximum(0,d2patch))
+            wgt = signal.convolve2d(wgt,boxFilter(f-1),mode='same')
+            outputImage += np.expand_dims(wgt,axis=2) * ngb
+            wgtsum += wgt
+    outputImage /= np.expand_dims(wgtsum,axis=2)
+
+    # Display the images
+    cv2.imshow("denoised_image",outputImage)
+    cv2.imshow("base_image",img)
+    cv2.imshow("base_image_variance",img_variance)
+
+    # Save the denoised image
+    cv2.imwrite("denoised_image.png", outputImage)
+
+    # Wait until we are done
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
